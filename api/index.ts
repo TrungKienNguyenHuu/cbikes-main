14115dd1f4596a78bb134fd25bcc930d89d50adc
Nguyen Huu Trung Kien <Kien.NHT215216@sis.hust.edu.vn>
2026-06-11 02:06:20 +0700

    api route test 3

--- api/index.ts
+++ api/index.ts
@@ -1,9 +1,10 @@
-// api/index.ts
 import appModule from "../backend/dist/app.js";
 
 const app = (appModule as any).default || appModule;
 
 export default function handler(req: any, res: any) {
   req.url = req.url.replace(/^\/api/, "") || "/";
+  req.originalUrl = req.url;
+
   return app(req, res);
 }
