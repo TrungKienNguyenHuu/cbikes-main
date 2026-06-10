146a44bab5726cbe3d254058017cbe63db964219
Nguyen Huu Trung Kien <Kien.NHT215216@sis.hust.edu.vn>
2026-06-11 01:30:28 +0700

    another fix

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -14,8 +14,9 @@ const PORT = process.env.PORT || 5001;
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.json({ message: "Express app loaded" });
+app.use((req, res, next) => {
+  console.log("PATH:", req.path);
+  next();
 });
 
 
