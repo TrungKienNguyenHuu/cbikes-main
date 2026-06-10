d42bec6c7af8a44a044dd953b42640e6933b4a91
Nguyen Huu Trung Kien <Kien.NHT215216@sis.hust.edu.vn>
2026-06-11 01:50:57 +0700

    api url test

--- api/index.ts
+++ api/index.ts
@@ -1,17 +1,9 @@
+// api/index.ts
 import appModule from "../backend/dist/app.js";
 
 const app = (appModule as any).default || appModule;
 
 export default function handler(req: any, res: any) {
-  console.log("typeof app:", typeof app);
-
-  if (typeof app !== "function") {
-    return res.status(500).json({
-      error: "App is not a function",
-      type: typeof app,
-      keys: Object.keys(app || {})
-    });
-  }
-
+  req.url = req.url.replace(/^\/api/, "") || "/";
   return app(req, res);
 }
