ed0ee66100d14669cc6d6b9c1ca7c15bcc332ff2
Nguyen Huu Trung Kien <Kien.NHT215216@sis.hust.edu.vn>
2026-06-11 01:14:23 +0700

    final test

--- api/index.ts
+++ api/index.ts
@@ -1,9 +1,3 @@
 import appModule from "../backend/dist/app.js";
 
-export default function handler(req: any, res: any) {
-  res.json({
-    type: typeof appModule,
-    keys: Object.keys(appModule),
-    defaultType: typeof (appModule as any).default
-  });
-}
+export default (appModule as any).default;
